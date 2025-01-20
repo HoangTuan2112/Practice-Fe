a1637c9a710fdc0064dea38e4ce9de15a1813166
HoangTuan2112 <tuanmaiyeunguoido@gmail.com>
2025-01-20 10:48:02 +0700

    first commit

---
+++ .history/src/component/home/Home_20250110233617.js
@@ -0,0 +1,15 @@
+import HomeCity from "./HomeCity";
+import HomePlace from "./HomePlace";
+import HomeBanner from "./HomeBanner";
+
+function Home() {
+  return (
+    <>
+      <HomeBanner />
+      <HomeCity />
+      <HomePlace />
+    </>
+  );
+}
+
+export default Home;
